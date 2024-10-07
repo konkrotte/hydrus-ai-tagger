c0f56ec2d3ec57085693ab893206e16702a4c078
konkrotte <konkrotte@posteo.net>
2024-10-07 23:50:30 +0200

    fix formatting again

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,30 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
     Daemon {
-    /// Path to the model folder
-    #[arg(short, long)]
-    model_dir: path::PathBuf,
+        /// Path to the model folder
+        #[arg(short, long)]
+        model_dir: path::PathBuf,
 
-    /// The threshold for a tag to be used
-    #[arg(short, long, default_value_t = 0.35)]
-    threshold: f32,
-    }
+        /// The threshold for a tag to be used
+        #[arg(short, long, default_value_t = 0.35)]
+        threshold: f32,
+    },
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
     match args.command {
-        Commands::Daemon { model_dir, threshold } => {
+        Commands::Daemon {
+            model_dir,
+            threshold,
+        } => {
             let interrogator = Interrogator::init(model_dir)?;
             let image = ImageReader::open("./image.jpg")?;
-            println!("{:?}", interrogator.interrogate(image.decode()?, threshold)?);
+            println!(
+                "{:?}",
+                interrogator.interrogate(image.decode()?, threshold)?
+            );
         }
     }
     Ok(())
